a6251b3179f0f092ce11c955a3b27b4d716b488c
kanapuliAthavan <athavankanapuli@gmail.com>
2017-01-15 15:50:01 +0530

    Problem solution by Array destruction method. removed each duplicate elements in array and trying to compute the lone integer

--- Lone Integer/main.go
+++ Lone Integer/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 
@@ -17,22 +20,30 @@ func loop(times int) {
 	for i := 0; i < times; i++ {
 		var arrayLength int
 		fmt.Scan(&arrayLength)
-		array := make([]int, arrayLength)
+		array := make([]int, arrayLength, arrayLength+1)
 		for z := 0; z < arrayLength; z++ {
 			fmt.Scan(&array[z])
 		}
-		duplicateArray := make([]int, arrayLength)
-		copy(duplicateArray, array)
-
-		for i := 0; i < len(array); i++ {
-			temp := array[i]
-			for a, b := range array {
+		//Sort the array in increasing order
+		sort.Ints(array)
+		fmt.Println("sorted Array: ", array)
+		mySlice = append(mySlice, array...)
+		fmt.Println("My Slice : ", mySlice)
+		for i := 0; i < len(mySlice); i++ {
+			temp := mySlice[i]
+			for a, b := range mySlice {
 				if i != a && a >= i && temp == b {
 					count++
-					newSlice = append(mySlice, temp)
+					if count == 1 {
+						count = 0
+						newSlice = append(mySlice[:i+1], mySlice[i+2:]...)
+						copy(mySlice, newSlice)
+						fmt.Println("Deleted Slice : ", mySlice)
+					}
+					//newSlice = append(mySlice, temp)
 					fmt.Println(count)
-					fmt.Println(newSlice)
-					pop
+
+					//fmt.Println(mySlice)
 				}
 			}
 		}
