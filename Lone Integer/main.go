a0f2935561fb0a7b6ccab1e0aa2a70a6de4f5e79
kanapuliathavan <athavankanapuli@gmail.com>
2017-01-17 11:46:07 +0530

    Working Solution using hash maps

--- Lone Integer/main.go
+++ Lone Integer/main.go
@@ -1,70 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
 
 	var test int
 	fmt.Scan(&test)
 	loop(test)
-
+	fmt.Println("Press any key  to  quit")
+	end := make([]byte, 10)
+	if _, err := os.Stdin.Read(end); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func loop(times int) {
 
 	for i := 0; i < times; i++ {
 		arrayLength := 0
-
+		positiveCount := 0
 		fmt.Scan(&arrayLength)
 		arrayMap := make(map[int]int)
 		array := make([]int, arrayLength)
+		var output string
 		for j := 0; j < arrayLength; j++ {
 			fmt.Scan(&array[j])
 			arrayMap[array[j]]++
-
 		}
 		for key, value := range arrayMap {
 			if value == 1 {
-				fmt.Println(key)
-				break
+				output = output + " " + strconv.Itoa(key)
+				positiveCount++
+				continue
 			}
+
+		}
+		if positiveCount == 0 {
 			fmt.Println(-1)
+		} else {
+			fmt.Println(output)
+		}
+
+		end := make([]byte, 10)
+		if _, err := os.Stdin.Read(end); err != nil {
+			log.Fatal(err)
 		}
 	}
 
-	/*	count := 0
-		mySlice := make([]int, 0, 1)
-		newSlice := make([]int, 0, 1)
-		for i := 0; i < times; i++ {
-			var arrayLength int
-			fmt.Scan(&arrayLength)
-			array := make([]int, arrayLength, arrayLength+1)
-			for z := 0; z < arrayLength; z++ {
-				fmt.Scan(&array[z])
-			}
-			//Sort the array in increasing order
-			sort.Ints(array)
-			fmt.Println("sorted Array: ", array)
-			mySlice = append(mySlice, array...)
-			fmt.Println("My Slice : ", mySlice)
-			for i := 0; i < len(mySlice); i++ {
-				temp := mySlice[i]
-				for a, b := range mySlice {
-					if i != a && a >= i && temp == b {
-						count++
-						if count == 1 {
-							count = 0
-							newSlice = append(mySlice[:i+1], mySlice[i+2:]...)
-							copy(mySlice, newSlice)
-							fmt.Println("Deleted Slice : ", mySlice)
-						}
-						//newSlice = append(mySlice, temp)
-						fmt.Println(count)
-
-						//fmt.Println(mySlice)
-					}
-				}
-			}
-
-		}*/
 }
